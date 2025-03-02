cdfd18d46da94cc9b8b06ab434d88b6bdb6f0bf8
David Xiao <3964699+davxiao@users.noreply.github.com>
2025-03-01 17:59:42 -0800

    initial code template generated by Xcode wizard

---
+++ photo2/photo2App.swift
@@ -0,0 +1,17 @@
+//
+//  photo2App.swift
+//  photo2
+//
+//  Created by David Xiao on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct photo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
